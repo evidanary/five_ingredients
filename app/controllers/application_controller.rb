eaa1e91f06b442596fad97b686e3a1dc0ec2b8fb
Yash Ranadive <yash.ranadive@gmail.com>
2016-02-19 16:46:01 -0800

    making devise forward to recipes url after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Make Devise redirect after successful login
+  def after_sign_in_path_for(user)
+    recipes_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  # Add this to generate /api/URI
+  # See https://www.youtube.com/watch?v=36M2BSA2LYk
+  # namespace :api, defaults: {format: :json} do
   resources :ingredients
   resources :steps
   resources :recipes
   root to: 'visitors#index'
-  devise_for :users
+#  devise_for :users
+  devise_for :users,
+    :path => '',
+    :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup'}
+  # end
 end
